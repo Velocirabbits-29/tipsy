94b750b37534a257d0673b0f0cc3ec30dc8621ab
Jueun Yun <graceyunn@gmail.com>
2021-11-05 17:27:03 -0400

    Completed webpack config

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV,
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'build'), // where we're looking
+      publicPath: '/build',
+    },
+    // re-routing api fetch calls to localhost 3000 (which has access to express)
+    proxy: {
+      '/api': 'http://localhost:3000' // or use '/*' instead of '/api' to take care of all other fetch calls
+     }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: './index.html',
+  })
+  ],
+  module: {
+    rules: [
+      { 
+        test: /\.(js|jsx)$/, 
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+}
+
+
