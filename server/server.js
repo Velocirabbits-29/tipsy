225b66eb4c4d5e15d2e819bed2bfff4a381ca563
Jueun Yun <graceyunn@gmail.com>
2021-11-06 12:21:45 -0400

    Co-authored-by: Patrick Constant <PLConstant@users.noreply.github.com> Co-authored-by: yingliufilms <yingliufilms@users.noreply.github.com> Co-authored-by: Jennifer Chau <jenniferchau@users.noreply.github.com> Co-authored-by: alanrichardson7 <alanrichardson7@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -6,6 +6,14 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
 
+if (process.env.NODE_ENV === 'production') {
+  // statically serve everything in the build folder on the route '/build'
+  app.use('/build', express.static(path.join(__dirname, '../build')));
+  // serve index.html on the route '/'
+  app.get('/', (req, res) => {
+    return res.status(200).sendFile(path.join(__dirname, '../index.html'));
+  });
+}
 
 app.listen(PORT, (err, res) => {
   if (err) console.log(err);
