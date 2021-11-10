e39726f8fed6be1fb82a88e50bc8bab86b03f4a8
Michael Chin <mikechin37@gmail.com>
2021-11-10 10:44:31 -0500

    created SQL database and added new database folder

---
+++ server/createDatabase.js
@@ -0,0 +1,4 @@
+const PG_URI = 'postgres://ikdvjxln:e4L33pn0X5g52n5ULGN2zkVmSHHoZ05l@rajje.db.elephantsql.com/ikdvjxln'
+
+// terminal command to load database:
+// psql -d postgres://ikdvjxln:e4L33pn0X5g52n5ULGN2zkVmSHHoZ05l@rajje.db.elephantsql.com/ikdvjxln -f create-database.sql
