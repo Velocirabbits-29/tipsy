d8befe47db05c4ac6065018487dfe04e8bb205be
Robert Howton <robert.f.howton@gmail.com>
2021-11-10 19:53:54 -0500

    implemented basic user authentication

--- server/router.js
+++ server/router.js
@@ -9,51 +9,52 @@ const router = express.Router();
 // ALL API GET REQUESTS
 
 /*_______ROUTES TO /signup AND /login WERE COMMENTED OUT BECAUSE AUTHCONTROLLER FUNCTIONALITY IS NOT FINALIZED _______*/
-router.post('/signup', 
-authController.createUser,
-    (req, res) => res.status(200).send('Signup endpoint fired'));
+router.post('/signup', authController.createUser, (req, res) =>
+  res.status(200).send('Signup endpoint fired')
+);
 
-router.post('/login',
-authController.verifyUser,
-    (req, res) => res.status(200).json(res.locals.userId)); 
+router.post('/login', authController.verifyUser, (req, res) => {
+  console.log('login router fired')
+  const responseObj = {user: res.locals.user, userVerified: res.locals.userVerified};
+  res
+    .status(200)
+    .json(responseObj);
+});
 
 // router for main user submit function
-router.get('/handleSubmit',
-dranksController.handleSubmit,
-  (req, res) => res.status(200).send(res.locals.drinks)
+router.get('/handleSubmit', dranksController.handleSubmit, (req, res) =>
+  res.status(200).send(res.locals.drinks)
 );
 
 // routers for stretch goals (popular and I'm feeling lucky)
-router.get('/placeholderforPopular',
-dranksController.getPopular,
-  (req, res) => res.status(200).json(res.locals.popular)
+router.get('/placeholderforPopular', dranksController.getPopular, (req, res) =>
+  res.status(200).json(res.locals.popular)
 );
 
-router.get('/placeholderforRandom',
-dranksController.getRandom,
-  (req, res) => res.status(200).json(res.locals.randomDrink)
+router.get('/placeholderforRandom', dranksController.getRandom, (req, res) =>
+  res.status(200).json(res.locals.randomDrink)
 );
 
-// unused routers for other API functions in case we 
+// unused routers for other API functions in case we
 // end up wanting to use them
-router.get('/placeholderforByName',
-dranksController.getByName,
+router.get('/placeholderforByName', dranksController.getByName, (req, res) =>
+  res.status(200).json(res.locals.drinks)
+);
+
+router.get(
+  '/placeholderforByIngredients',
+  dranksController.getByIngredients,
   (req, res) => res.status(200).json(res.locals.drinks)
 );
 
-router.get('/placeholderforByIngredients',
-dranksController.getByIngredients,
+router.get(
+  '/placeholderforCategory',
+  dranksController.getByCategory,
   (req, res) => res.status(200).json(res.locals.drinks)
 );
 
-router.get('/placeholderforCategory',
-dranksController.getByCategory,
-  (req, res) => res.status(200).json(res.locals.drinks)
-);
-
-router.get('/placeholderforGlass',
-dranksController.getByGlass,
-  (req, res) => res.status(200).json(res.locals.drinks)
+router.get('/placeholderforGlass', dranksController.getByGlass, (req, res) =>
+  res.status(200).json(res.locals.drinks)
 );
 // END API GET REQUESTS
 
